3d17e0423f2ef294c51524181410170dc9313402
maximiliano <maxifava667@gmail.com>
2024-04-24 04:37:49 -0300

    getActualTime mas simple

--- server/helpers/getTime.c
+++ server/helpers/getTime.c
@@ -2,13 +2,11 @@
 #include <stdlib.h>
 
 char* getActualTime() {
-    struct tm fechaActual;
     time_t tiempoActual = time(NULL);
-    setenv("TZ", "GMT", 1); // Establecer la variable de entorno TZ en GMT
-    localtime_r(&tiempoActual, &fechaActual);
+    struct tm* fechaActual = gmtime(&tiempoActual);
 
     static char fechaHoraFormato[100];
-    strftime(fechaHoraFormato, sizeof(fechaHoraFormato), "%a, %d %b %Y %H:%M:%S %Z", &fechaActual);
+    strftime(fechaHoraFormato, sizeof(fechaHoraFormato), "%a, %d %b %Y %H:%M:%S %Z", fechaActual);
 
     return fechaHoraFormato;
 }
